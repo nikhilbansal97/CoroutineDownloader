a9a6cc4fcd20e8804614fb9bd8dc134a44b2869e
Nikhil Bansal <nikhil97.nb@gmail.com>
2019-08-07 11:25:23 +0530

    Fixed the notification channel and permission issue on Devices 8.1 and above. Added more screenshots
    Signed-off-by: Nikhil Bansal <nikhil97.nb@gmail.com>

--- app/src/main/java/com/app/nikhil/coroutinedownloader/utils/NotificationUtils.kt
+++ app/src/main/java/com/app/nikhil/coroutinedownloader/utils/NotificationUtils.kt
@@ -1,20 +1,40 @@
 package com.app.nikhil.coroutinedownloader.utils
 
 import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
 import androidx.core.app.NotificationCompat
 import javax.inject.Inject
 
 class NotificationUtils @Inject constructor(private val context: Context) {
 
   companion object {
-    private const val NOTIFICATION_CHANNEL = "CHANNEL-001"
+    private const val CHANNEL_ID = "CHANNEL-001"
+    private const val CHANNEL_NAME = "CoroutineDownloader Channel"
   }
 
-  fun createNotification(title: String, text: String): Notification {
-    return NotificationCompat.Builder(context, NOTIFICATION_CHANNEL)
+  fun createNotification(
+    title: String,
+    text: String
+  ): Notification {
+    val builder = NotificationCompat.Builder(context)
         .setContentTitle(title)
         .setContentText(text)
-        .build()
+
+    if (VERSION.SDK_INT >= VERSION_CODES.O) {
+      val notificationManager = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+      notificationManager.createNotificationChannel(
+          NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT)
+      )
+      builder.apply {
+        setChannelId(CHANNEL_ID)
+        setOngoing(true)
+      }
+    }
+    return builder.build()
   }
 }
